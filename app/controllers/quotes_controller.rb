7f9e19acb43e6cc579cbdc6e708fb4e20e40abd8
Stephen <stephenmelnick8@gmail.com>
2019-05-31 18:15:04 +0000

    built the about page

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -12,7 +12,11 @@ end
   if @quote.invalid?
     flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
   end
+
      redirect_to root_path
+  end
+
+  def about
   end
 
   private
